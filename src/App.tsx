b2b2f21b0383cbe4392d08ba5a891dd95e355c58
Jacqueline Cardeal <jacquelinecardeal@hotmail.com>
2026-02-26 07:18:07 -0300

    Atualização de componentes

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,39 @@ import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
 import Cadastro from "./pages/cadastro/Cadastro";
 import Login from "./pages/login/Login";
+import { AuthProvider } from "./contexts/AuthContext";
+import ListaTemas from "./components/tema/listatemas/ListaTemas";
+import FormTema from "./components/tema/formtema/FormTema";
+import DeletarTema from "./components/tema/deletartema/DeletarTema";
+import ListaPostagens from "./components/postagem/listapostagens/ListaPostagens";
+import FormPostagem from "./components/postagem/formpostagem/FormPostagem";
+import DeletarPostagem from "./components/postagem/deletarpostagem/DeletarPostagem";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <div className="min-h-[80vh]">
-            <Routes>
-              <Route path="/" element={<Login/>} />
-              <Route path="/home" element={<Home/>} />
-              <Route path="/cadastro" element={<Cadastro/>} />
-            </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
-
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+              <Routes>
+                <Route path="/" element={<Login/>} />
+                <Route path="/home" element={<Home/>} />
+                <Route path="/cadastro" element={<Cadastro/>} />
+                {/* <Route path="/login" element={<Login />} /> */}
+                <Route path="/temas" element={<ListaTemas />} />
+                <Route path="/cadastrartema" element={<FormTema />} />
+                <Route path="/editartema/:id" element={<FormTema />} />
+                <Route path="/deletartema/:id" element={<DeletarTema />} />
+                <Route path="/postagens" element={<ListaPostagens />} />
+                <Route path="/cadastrarpostagem" element={<FormPostagem />} />
+                <Route path="/editarpostagem" element={<FormPostagem />} />
+                <Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
+              </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </>
   );
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+// import React from 'react';
+import ListaPostagens from '../../components/postagem/listapostagens/ListaPostagens';
+import ModalPostagem from '../../components/postagem/modalpostagem/ModalPostagem';
 
 function Home() {
   return (
@@ -13,8 +15,7 @@ function Home() {
                     <p className='text-xl'> Expresse aqui seus pensamentos e opiniões </p>
 
                     <div className='flex justify-around gap-4'>
-                        <div className='rounded text-white border-white border-solid border-2 py-2 px-4'> Nova Postagem </div>
-                
+                        <ModalPostagem />
                     </div>
                 </div>
 
@@ -25,6 +26,7 @@ function Home() {
                 </div>
             </div>
         </div>
+        <ListaPostagens />
     </>
   )
 }
